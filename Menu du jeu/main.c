bf74256cb183b62abd7fea5cba86eb407ffd23ec
KeyyKeyyKeyy__ <95491291+KeyyKeyyKeyy@users.noreply.github.com>
2022-05-16 20:22:50 +0200

    Update main.c
    Modifications faites dû au changement de la taille de l'écran.

--- Menu du jeu/main.c
+++ Menu du jeu/main.c
@@ -1,9 +1,8 @@
-#include "header.h"
+#include "maBiblioJeu.h"
 
 void start()
 {
     show_mouse(screen);
-    int i=0;
 
     BITMAP* imageMenu1;
     imageMenu1 = load_bitmap("Image menu chargement 1.bmp",NULL);
@@ -85,63 +84,74 @@ void start()
         exit(EXIT_FAILURE);
     }
     /******************************************************************************/
-    for(i=0; i<1; i++){
-
-        blit(imageMenu1,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu1,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
 
-        blit(imageMenu2,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement.", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu2,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement.", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
 
-        blit(imageMenu3,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement..", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu3,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement..", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
 
-        blit(imageMenu4,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement...", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu4,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement...", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
 
-        blit(imageMenu5,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement.", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu5,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement.", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
 
-        blit(imageMenu6,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement..", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
+    blit(imageMenu6,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement..", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
-        blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
-        textout_centre_ex(screen, font, "Chargement...", 400, 300, makecol(0, 0, 0), -1);
-        sleep(1);
-    }
+    blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Chargement...", 512, 384, makecol(0, 0, 0), -1);
+    sleep(1);
 
-    blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
-    textout_centre_ex(screen, font, "Appyuez sur Start pour commencer.", 400, 300, makecol(255, 255, 255), -1);
-    draw_sprite(screen, boutonStart, 300, 350);
+
+    blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "Appyuez sur Start pour commencer.", 512, 384, makecol(255, 255, 255), -1);
+    draw_sprite(screen, boutonStart, 415, 434);
+    rectfill(screen,0,0,40,20,makecol(255,255,0));
+    textprintf_ex(screen,font,4,6,makecol(0,0,0),makecol(255,0,0),"EXIT");
 
 
     int clique = 0;
     while (clique == 0){
         //Si la souris est sur le bouton
-        if(mouse_x>=300 && mouse_x<=470 && mouse_y>=350 && mouse_y<=448 && (mouse_b & 1))
+        if (mouse_b & 1)
         {
-            //Suppresion des anciennes bitmaps sauf la 7 qui reste pour le fond d'écran
-            destroy_bitmap(imageMenu1);
-            destroy_bitmap(imageMenu2);
-            destroy_bitmap(imageMenu3);
-            destroy_bitmap(imageMenu4);
-            destroy_bitmap(imageMenu5);
-            destroy_bitmap(imageMenu6);
-            destroy_bitmap(boutonStart);
-            //Appel de la fonction pour le choix du nombre de joueurs
-            choix_player();
-            clique=1;
+            if(mouse_x>=415 && mouse_x<=585 && mouse_y>=434 && mouse_y<=532 && (mouse_b & 1))
+            {
+                //Suppresion des anciennes bitmaps sauf la 7 qui reste pour le fond d'Ã©cran
+                destroy_bitmap(imageMenu1);
+                destroy_bitmap(imageMenu2);
+                destroy_bitmap(imageMenu3);
+                destroy_bitmap(imageMenu4);
+                destroy_bitmap(imageMenu5);
+                destroy_bitmap(imageMenu6);
+                destroy_bitmap(boutonStart);
+                //Appel de la fonction pour le choix du nombre de joueurs
+                choix_player();
+                clique=1;
+            }
+            if (mouse_x<=40 && mouse_y<=20)
+            {
+                // prise en compte effective de la zone cliquable EXIT :
+                // clic gauche en haut Ã  gauche de l'Ã©cran -> fin du programme
+                allegro_message("QUOIIIII !!! Tu quittes notre jeu ?? Nahhhh ca doit etre une erreur tu as mal clique aller revient jouer (c'est un ordre).");
+                allegro_exit();
+                exit(EXIT_FAILURE);
+            }
         }
     }
     destroy_bitmap(imageMenu7);
@@ -204,70 +214,79 @@ void choix_player()
     }
     /******************************************************************************/
 
-
-    blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
-    textout_centre_ex(screen, font, "A combien voulez vous jouer ?", 400, 300, makecol(255, 255, 255), -1);
+    blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
+    textout_centre_ex(screen, font, "A combien voulez vous jouer ?", 512, 384, makecol(255, 255, 255), -1);
 
     //Affichage des 3 boutons pour le nombres de joueurs
-    draw_sprite(screen, bouton2p, 90, 350);
-    draw_sprite(screen, bouton3p, 300, 350);
-    draw_sprite(screen, bouton4p, 510, 350);
+    draw_sprite(screen, bouton2p, 106, 440);
+    draw_sprite(screen, bouton3p, 412, 440);
+    draw_sprite(screen, bouton4p, 718, 440);
 
+    rectfill(screen,0,0,40,20,makecol(0,0,0));
+    textprintf_ex(screen,font,4,6,makecol(255,255,0),makecol(255,0,0),"EXIT");
 
     sleep(1);
 
     int clic = 0;
     while (clic == 0){
         //Si la souris est sur le bouton "2 joueurs" et qu'il y a un clic
-        if (mouse_b & 1){
-            if((mouse_x>=90) && (mouse_x<=290) && (mouse_y>=353) && (mouse_y<=422))
+        if (mouse_b & 1)
+        {
+            if(mouse_x>=106 && mouse_x<=306 && mouse_y>=440 && mouse_y<=517)
             {
-                blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
+                blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
                 destroy_bitmap(bouton2p);
                 destroy_bitmap(bouton3p);
                 destroy_bitmap(bouton4p);
-                draw_sprite(screen, casenomjoueur, 100, 150);
-                draw_sprite(screen, casenomjoueur, 100, 350);
-                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 170, makecol(255, 255, 255), -1);
-                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 370, makecol(255, 255, 255), -1);
+                draw_sprite(screen, casenomjoueur, 100, 160);
+                draw_sprite(screen, casenomjoueur, 100, 440);
+                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 180, makecol(255, 255, 255), -1);
+                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 460, makecol(255, 255, 255), -1);
                 clic=1;
                 sleep(50);
             }
-            if(mouse_x>=300 && mouse_x<=500 && mouse_y>=350 && mouse_y<=422)
+            if(mouse_x>=412 && mouse_x<=612 && mouse_y>=440 && mouse_y<=517)
             {
-                blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
+                blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
                 destroy_bitmap(bouton2p);
                 destroy_bitmap(bouton3p);
                 destroy_bitmap(bouton4p);
-                draw_sprite(screen, casenomjoueur, 100, 75);
-                draw_sprite(screen, casenomjoueur, 100, 250);
+                draw_sprite(screen, casenomjoueur, 100, 110);
+                draw_sprite(screen, casenomjoueur, 100, 310);
+                draw_sprite(screen, casenomjoueur, 100, 510);
+                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 130, makecol(255, 255, 255), -1);
+                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 330, makecol(255, 255, 255), -1);
+                textout_centre_ex(screen, font, "Entrez le nom du troisieme joueur :", 260, 530, makecol(255, 255, 255), -1);
+                clic=1;
+                sleep(50);
+            }
+            if(mouse_x>=718 && mouse_x<=918 && mouse_y>=440 && mouse_y<=517)
+            {
+                blit(imageMenu7,screen, 0, 0, 0, 0, 1024, 768);
+                destroy_bitmap(bouton2p);
+                destroy_bitmap(bouton3p);
+                destroy_bitmap(bouton4p);
+                draw_sprite(screen, casenomjoueur, 100, 70);
+                draw_sprite(screen, casenomjoueur, 100, 245);
                 draw_sprite(screen, casenomjoueur, 100, 425);
-                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 95, makecol(255, 255, 255), -1);
-                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 270, makecol(255, 255, 255), -1);
+                draw_sprite(screen, casenomjoueur, 100, 590);
+                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 90, makecol(255, 255, 255), -1);
+                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 265, makecol(255, 255, 255), -1);
                 textout_centre_ex(screen, font, "Entrez le nom du troisieme joueur :", 260, 445, makecol(255, 255, 255), -1);
+                textout_centre_ex(screen, font, "Entrez le nom du quatrieme joueur :", 260, 610, makecol(255, 255, 255), -1);
                 clic=1;
                 sleep(50);
             }
-            if(mouse_x>=510 && mouse_x<=710 && mouse_y>=350 && mouse_y<=422)
+            if (mouse_x<=40 && mouse_y<=20)
             {
-                blit(imageMenu7,screen, 0, 0, 0, 0, 800, 800);
-                destroy_bitmap(bouton2p);
-                destroy_bitmap(bouton3p);
-                destroy_bitmap(bouton4p);
-                draw_sprite(screen, casenomjoueur, 100, 0);
-                draw_sprite(screen, casenomjoueur, 100, 150);
-                draw_sprite(screen, casenomjoueur, 100, 300);
-                draw_sprite(screen, casenomjoueur, 100, 450);
-                textout_centre_ex(screen, font, "Entrez le nom du premier joueur :", 260, 20, makecol(255, 255, 255), -1);
-                textout_centre_ex(screen, font, "Entrez le nom du deuxieme joueur :", 260, 170, makecol(255, 255, 255), -1);
-                textout_centre_ex(screen, font, "Entrez le nom du troisieme joueur :", 260, 320, makecol(255, 255, 255), -1);
-                textout_centre_ex(screen, font, "Entrez le nom du quatrieme joueur :", 260, 470, makecol(255, 255, 255), -1);
-                clic=1;
-                sleep(50);
+                allegro_message("QUOIIIII !!! Tu quittes notre jeu ?? Nahhhh ca doit etre une erreur tu as mal clique aller revient jouer (c'est un ordre)");
+                allegro_exit();
+                exit(EXIT_FAILURE);
             }
         }
     }
 }
+
 
 int main()
 {
@@ -278,10 +297,10 @@ int main()
         printf("%ca marche pas, demande %c Cyrk%c5391 sur Discord", 128 ,133, 23);
         return 1;
     }
-    show_mouse(screen); ///Permet de montrer la souris à l'écran
+    show_mouse(screen); ///Permet de montrer la souris Ã  l'Ã©cran
 
     set_color_depth(desktop_color_depth());
-    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,1024,768,0,0)!=0)
     {
         allegro_message("Probl%cme graphique", 138);
         allegro_exit();
@@ -291,5 +310,4 @@ int main()
     start();
 
     return 0;
-
 }END_OF_MAIN();
